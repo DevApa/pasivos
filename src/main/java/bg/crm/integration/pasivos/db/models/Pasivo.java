e43e61a4514497d60cecfbd59251e03929480ef5
carlos aparicio <apariciovivero@gmail.com>
2025-04-22 22:23:18 -0500

    lombok reference

--- src/main/java/bg/crm/integration/pasivos/db/models/Pasivo.java
+++ src/main/java/bg/crm/integration/pasivos/db/models/Pasivo.java
@@ -1,5 +1,17 @@
 package bg.crm.integration.pasivos.db.models;
 
-public class Pasivo {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class Pasivo {
+    private String id;
+    private String name;
+    private String description;
+    private String status;
 }
