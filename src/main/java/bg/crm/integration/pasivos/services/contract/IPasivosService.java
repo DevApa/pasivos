8a700802ff57365cf278a870df396c39bb39a870
carlos aparicio <apariciovivero@gmail.com>
2025-04-22 22:08:44 -0500

    delete import not use

--- src/main/java/bg/crm/integration/pasivos/services/contract/IPasivosService.java
+++ src/main/java/bg/crm/integration/pasivos/services/contract/IPasivosService.java
@@ -2,8 +2,6 @@ package bg.crm.integration.pasivos.services.contract;
 
 import java.util.List;
 
-import org.springframework.stereotype.Service;
-
 import bg.crm.integration.pasivos.controller.dto.PasivoDTO;
 
 
